bfd4b38d1406e2ff7525895c08181141b11402d8
NCdev1706 <netcradusdeveloper@gmail.com>
2026-09-09 12:08:05 +0530

    fix complaint model import

--- lib/providers/admin_provider.dart
+++ lib/providers/admin_provider.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:dairy_app/models/complaint_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
